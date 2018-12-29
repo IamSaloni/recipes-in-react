d8ef99a09b3b8a7af8d20ec0c1e53f5a2b1e57d1
Saloni Sharma <sharmasaloni061@gmail.com>
2018-12-29 16:43:07 +0530

    initialized react application

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import log from './log';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Print extends React.Component {
+    render () {
+        return (
+            <div >
+                <p>Hello World</p>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render (
+    <Print />,document.getElementById('root')
+ );
 
 
+// ReactDOM.render (
+//    React.createElement('h1', {className: 'kk' }, 'hello saloni'),document.getElementById('root')
+// );
 
-log ('Hello World!')
-
+// React.createElement('h1', {className: 'kk' }, 'hello gugu')
