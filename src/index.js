6275c80366edcfe46613f76b63144fa1cee9a4d9
Saloni Sharma <sharmasaloni061@gmail.com>
2018-12-29 18:26:19 +0530

    Hello greeting added with the dynamic input

--- src/index.js
+++ src/index.js
@@ -2,17 +2,43 @@ import React, {Fragment} from 'react'
 import ReactDOM from 'react-dom'
 
 class Print extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name : this.props.name
+        }
+    }
+
+    handleChange(e){
+        this.setState ({
+            name : e.target.value
+        })
+    }
+
     render () {
         return (
             <div >
-                <p>Hello World</p>
+                <label htmlFor="firstname">
+                    Name:
+                </label>
+                <input
+                    type="text"
+                    id="firstname"
+                    name="firstname"
+                    placeholder="Enter Name"
+                    onChange={() => this.handleChange(event)}
+                />
+                <p>Hello {this.state.name}</p>
             </div>
         )
     }
 }
 
 ReactDOM.render (
-    <Print />,document.getElementById('root')
+    <Print name="Customer" />,document.getElementById('root')
  );
 
 
+//Create a component 
+//Which greets the customer
+//It has Input to enter a name for greeting 
