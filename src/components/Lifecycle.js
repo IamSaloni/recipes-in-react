9960d34f4b7e6e01403a9ebb90e4ffc727426141
Saloni Sharma <sharmasaloni061@gmail.com>
2018-12-31 17:34:14 +0530

    added lifecycle method

--- src/components/Lifecycle.js
+++ src/components/Lifecycle.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 class Lifecycle extends React.Component {
+        componentWillMount() {
+            console.log('Component is about to mount')
+        }
+        componentDidMount(){
+            console.log('Component has just mounted')
+        }
     render() {
         return (
             <div className="react-life"></div>
