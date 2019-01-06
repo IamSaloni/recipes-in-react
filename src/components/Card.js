8eb315ccbb38ac2c9ad7f2d5e209c0da8f3cffd6
Saloni Sharma <sharmasaloni061@gmail.com>
2019-01-06 23:21:33 +0530

    using fetch API got github data

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,48 +2,39 @@ import React from 'React'
 
 
 class Card extends React.Component {
+
     constructor(props) {
         super(props);
             this.state={
-                 newData: " "
+                name:' ',
+                company:'',
+                avatar_url:''
+            }
+        }
+            componentDidMount(){
+                fetch('http://localhost:8080/src/avtar.json')
+                .then(response=>response.json())
+                .then(data=>{
+                    this.setState({
+                        name:data.name,
+                        company:data.company,
+                        avatar_url:data.avatar_url
+                    })
+                })
             }
 
-    componentDidMount() {
-            fetch(`http://localhost:8080/src/avtar.json`)
-            .then(results =>{
-                return results.json();
-            })
-            .then(data => {
-                return let newData=data.results.map((newData)=> {
-                        return(
-                            <div key={newData.results}>
-                            <img src={"https://avatars3.githubusercontent.com/u/38960717?s=460&v=4"}/>
-                            <h1>
-                                <span>{newData.name}</span>
-                                <span>{newData.company}</span>
-                            </h1>
-                            </div>
-                        )
-                })
-            })
-
-            this.setState({
-                newData:newData
-            });
-    }
-        render() {
+         render() {
             return (
                 <div>
-                    <img src="https://avatars3.githubusercontent.com/u/38960717?s=460&v=4"/>
+                    <img src={this.state.avatar_url}/>
                     <h1>
                         <span>{this.state.name}</span>
                         <span>{this.state.company}</span>
                     </h1>
-                    {this.state.newData}
                 </div>
             )
         }
     }
-}
+
 
 export default Card
